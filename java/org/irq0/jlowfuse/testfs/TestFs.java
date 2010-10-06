4d2ab548eb489fcc444b718cfcd21d3a5c04f64c
Marcel Lauhoff <ml@irq0.org>
2010-10-07 00:51:25 +0200

    started testfs class

---
+++ java/org/irq0/jlowfuse/testfs/TestFs.java
@@ -0,0 +1,12 @@
+package org.irq0.jlowfuse.testfs;
+
+import org.irq0.jlowfuse.*;
+
+
+public class TestFs 
+{
+        public static void main(String args[]) 
+                {
+                        
+                      
+}
