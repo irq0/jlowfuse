9cd4d2f057b15c2219e1ee8c4ba0c9a74f162bff
Marcel Lauhoff <ml@irq0.org>
2010-10-08 01:14:17 +0200

    output of return code

--- testfs.sh
+++ testfs.sh
@@ -3,8 +3,9 @@
     cd dist
     echo "Mounting..."
     java -Djava.library.path=. -jar testfs.jar
+    ret=$?
     echo
-    echo 
+    echo "Return code: $ret"
     echo "... umounting"
     sudo umount /mnt1
 
