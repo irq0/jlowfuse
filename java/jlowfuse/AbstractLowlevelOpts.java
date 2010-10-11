1311c69162775a2aa6de951cfaed511b79d37de4
Marcel Lauhoff <ml@irq0.org>
2010-10-11 21:43:41 +0200

    made all opts public

--- java/jlowfuse/AbstractLowlevelOpts.java
+++ java/jlowfuse/AbstractLowlevelOpts.java
@@ -17,96 +17,96 @@ public abstract class AbstractLowlevelOpts {
         return;
     }
 
-    void destroy(ByteBuffer data) {
+    public void destroy(ByteBuffer data) {
         return;
     }
 
-    void lookup(FuseReq req, long parent, String name) {
+    public void lookup(FuseReq req, long parent, String name) {
         return;
     }
 
-    void forget(FuseReq req, long ino, long nlookup) {
+    public void forget(FuseReq req, long ino, long nlookup) {
         return;
     }
 
-    void getattr(FuseReq req, long ino) {
+    public void getattr(FuseReq req, long ino) {
         return;
     }
 
-    void setattr(FuseReq req, long ino, stat stat, int to_set) {
+    public void setattr(FuseReq req, long ino, stat stat, int to_set) {
         return;
     }
 
-    void readlink(FuseReq req, long ino) {
+    public void readlink(FuseReq req, long ino) {
         return;
     }
 
-    void mknod(FuseReq req, long parent, String name, short mode, short rdev) {
+    public void mknod(FuseReq req, long parent, String name, short mode, short rdev) {
         return;
     }
 
-    void mkdir(FuseReq req, long parent, String name, short mode) {
+    public void mkdir(FuseReq req, long parent, String name, short mode) {
         return;
     }
 
-    void unlink(FuseReq req, long parent, String name) {
+    public void unlink(FuseReq req, long parent, String name) {
         return;
     }
 
-    void rmdir(FuseReq req, long parent, String name) {
+    public void rmdir(FuseReq req, long parent, String name) {
         return;
     }
 
-    void symlink(FuseReq req, String link, long parent, String name) {
+    public void symlink(FuseReq req, String link, long parent, String name) {
         return;
     }
 
-    void rename(FuseReq req, long parent, String name,
+    public void rename(FuseReq req, long parent, String name,
                 long newparent, String newname) {
         return;
     }
 
-    void link(FuseReq req, long ino, long newparent, String newname) {
+    public void link(FuseReq req, long ino, long newparent, String newname) {
         return;
     }
 
-    void open(FuseReq req, long ino) {
+    public void open(FuseReq req, long ino) {
         return;
     }
 
-    void read(FuseReq req, long ino, int size, int off) {
+    public void read(FuseReq req, long ino, int size, int off) {
         return;
     }
 
-    void write(FuseReq req, long ino, ByteBuffer buf, int size, int off) {
+    public void write(FuseReq req, long ino, ByteBuffer buf, int size, int off) {
         return;
     }
 
-    void flush(FuseReq req, long ino) {
+    public void flush(FuseReq req, long ino) {
         return;
     }
 
-    void release(FuseReq req, long ino) {
+    public void release(FuseReq req, long ino) {
         return;
     }
 
-    void fsync(FuseReq req, long ino, int datasync) {
+    public void fsync(FuseReq req, long ino, int datasync) {
         return;
     }
 
-    void opendir(FuseReq req, long ino) {
+    public void opendir(FuseReq req, long ino) {
         return;
     }
 
-    void readdir(FuseReq req, long ino, int size, int off) {
+    public void readdir(FuseReq req, long ino, int size, int off) {
         return;
     }
 
-    void releasedir(FuseReq req, long ino) {
+    public void releasedir(FuseReq req, long ino) {
         return;
     }
 
-    void fsyncdir(FuseReq req, long ino, int datasync) {
+    public void fsyncdir(FuseReq req, long ino, int datasync) {
         return;
     }
 
@@ -114,49 +114,49 @@ public abstract class AbstractLowlevelOpts {
         return;
     }
 
-    void setxattr(FuseReq req, long ino, String name,
+    public void setxattr(FuseReq req, long ino, String name,
                   ByteBuffer value, int size, int flags) {
         return;
     }
 
-    void getxattr(FuseReq req, long ino, String name, int size) {
+    public void getxattr(FuseReq req, long ino, String name, int size) {
         return;
     }
 
-    void listxattr(FuseReq req, long ino, int size) {
+    public void listxattr(FuseReq req, long ino, int size) {
         return;
     }
 
-    void removexattr(FuseReq req, long ino, String name) {
+    public void removexattr(FuseReq req, long ino, String name) {
         return;
     }
 
-    void access(FuseReq req, long ino, int mask) {
+    public void access(FuseReq req, long ino, int mask) {
         return;
     }
 
-    void create(FuseReq req, long parent, String name, short mode) {
+    public void create(FuseReq req, long parent, String name, short mode) {
         return;
     }
 
     /*    
-    void getlk(long ino, Flock lock) {
+    public void getlk(long ino, Flock lock) {
     }
 
-    void setlk(long ino, Flock lock, int sleep) {
+    public void setlk(long ino, Flock lock, int sleep) {
     }
     */
 
-    void bmap(FuseReq req, long ino, int blocksize, long idx) {
+    public void bmap(FuseReq req, long ino, int blocksize, long idx) {
         return;
     }
 
     /*
-    void ioctl(long ino, int cmd, ByteBuffer arg,
+    public void ioctl(long ino, int cmd, ByteBuffer arg,
     */
 
     /*
-    void poll(long ino, PollHandle ph) {
+    public void poll(long ino, PollHandle ph) {
     }
     */
 }
