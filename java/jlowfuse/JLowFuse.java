71fe499daafb06170f2dff69392e8cf36658ed6c
Marcel Lauhoff <ml@irq0.org>
2010-10-12 00:34:41 +0200

    setOpsObject native call

--- java/jlowfuse/JLowFuse.java
+++ java/jlowfuse/JLowFuse.java
@@ -4,6 +4,15 @@ import fuse.*;
 
 public class JLowFuse {
     public native int init(Object opts);
+    private native SWIGTYPE_p_fuse_lowlevel_ops setOpsObject(AbstractLowlevelOpts ops);
+
+    public Session lowlevelNew(AbstractLowlevelOpts ops) {
+        SWIGTYPE_p_fuse_lowlevel_ops ops_p;
+
+        ops_p = setOpsObject(ops);
+
+        fuse_lowlevel_new(null, ops_p, sizeof(ops_p), null); // will not work :(
+    }
     
     static {
         System.loadLibrary("jlowfuse");
