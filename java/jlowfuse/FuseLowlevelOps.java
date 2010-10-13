fb5c1ed7b39454c451c0b7a6d8059cb678d9b6b9
Marcel Lauhoff <ml@irq0.org>
2010-10-13 23:47:36 +0200

    FuseLowlevelOps wrapps SWIGTYPE_p_fuse_lowlevel_ops

---
+++ java/jlowfuse/FuseLowlevelOps.java
@@ -0,0 +1,9 @@
+package jlowfuse;
+
+import fuse.*;
+
+public class FuseLowlevelOps extends SWIGTYPE_p_fuse_lowlevel_ops {
+    public FuseLowlevelOps(long ptr) {
+        super(ptr, false);
+    }
+}
