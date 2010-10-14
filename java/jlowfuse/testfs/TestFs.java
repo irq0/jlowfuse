8c4dab31247eb60fdf8896faf082b3e141d3e98e
Marcel Lauhoff <ml@irq0.org>
2010-10-14 04:02:09 +0200

    fuse initialisierung (mount, eventloop, ..) im testfs  impl

--- java/jlowfuse/testfs/TestFs.java
+++ java/jlowfuse/testfs/TestFs.java
@@ -1,13 +1,32 @@
 package jlowfuse.testfs;
 
+import fuse.*;
 import jlowfuse.*;
 import java.util.*;
 
 public class TestFs {
-    public static void main(String args[]) {
-        JLowFuse f = new JLowFuse();
+    static {
+        System.loadLibrary("jlowfuse");
+    }
 
-        f.lowlevelNew(new TestFsOpts());
+    public static void main(String args[]) {
+
+        SWIGTYPE_p_fuse_chan chan = fuse.fuse_mount("/mnt1", new FuseArgs());        
+        JLowFuse f = new JLowFuse();
+        SWIGTYPE_p_fuse_session sess = f.lowlevelNew(new TestFsOpts());
+
+        //        fuse.fuse_set_signal_handlers(sess);
+        fuse.fuse_session_add_chan(sess, chan);
+        int err = fuse.fuse_session_loop(sess);
+
+        //        fuse.fuse_remove_signal_handlers(sess);
+        fuse.fuse_session_remove_chan(chan);
+
+        fuse.fuse_session_destroy(sess);
+        fuse.fuse_session_exit(sess);
+
+        fuse.fuse_unmount("/mnt1", chan);
+        
     }
       
 }
