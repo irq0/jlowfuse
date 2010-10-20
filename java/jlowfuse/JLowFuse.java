1a4e7e631b974ca54b443ad0795fab3a1b1e97a5
Marcel Lauhoff <ml@irq0.org>
2010-10-21 00:03:34 +0200

    fix

--- java/jlowfuse/JLowFuse.java
+++ java/jlowfuse/JLowFuse.java
@@ -4,16 +4,25 @@ import fuse.*;
 
 public class JLowFuse {
     public native int init(Object opts);
-    private static native long setOps(AbstractLowlevelOps ops);
-    
-    public static SWIGTYPE_p_fuse_session lowlevelNew(FuseArgs args, LowlevelOps ops_object) {
-        long ops_l;
-        ops_l = JLowFuse.setOps(ops_object);
+    private static native long setOps(LowlevelOpsProxy ops);
 
-        FuseLowlevelOps ops = new FuseLowlevelOps(ops_l);
+    /**
+     * Establish new lowlevel session.
+     * Registers `LowlevelOps` in fuse c api and calles fuse_lowlevel_new
+     *
+     * @param args Fuse Args eg. Commandline
+     * @param ops  LowlevelOps instance - implements filesystem operations
+     * @return Opaque pointer to fuse_session type
+     */
+    public static SWIGTYPE_p_fuse_session lowlevelNew(FuseArgs args, LowlevelOps ops) {
+        LowlevelOpsProxy proxy = new LowlevelOpsProxy();
+        proxy.register(ops);
+        
+        long ops_p = JLowFuse.setOps(proxy);
+        FuseLowlevelOps ops_f = new FuseLowlevelOps(ops_p);
 
         return fuse.fuse_lowlevel_new(args,
-                                      ops, 144 , null); /* 144 = sizeof(fuse ops) */
+                                      ops_f, 144 , null); /* 144 = sizeof(fuse ops) */
     }
 
     
