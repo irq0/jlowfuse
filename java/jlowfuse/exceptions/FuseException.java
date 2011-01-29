e670acff5ac6f3923b424f7f18d395740d7e4926
Marcel Lauhoff <ml@irq0.org>
2011-01-29 18:08:16 +0100

    Exceptions base class and eclipse project file

---
+++ java/jlowfuse/exceptions/FuseException.java
@@ -0,0 +1,15 @@
+package jlowfuse.exceptions;
+
+public class FuseException extends Exception {
+    int errno = 0;
+    
+    private static final long serialVersionUID = -7429088074385678308L;
+
+    public FuseException(int errno) {
+        this.errno = errno;
+    }
+    
+    public getErrno() {
+        return errno;
+    }
+}
