9dd8ff6648ac819cde8545d3389b05a9f8f754a8
Marcel Lauhoff <ml@irq0.org>
2011-09-25 22:40:18 +0200

    Moved default task implementations to subpackage

--- java/jlowfuse/async/tasks/DefaultInit.java
+++
@@ -1,6 +0,0 @@
-package jlowfuse.async.tasks;
-
-public class DefaultInit extends JLowFuseTask implements Init {
-	public void attributes() {
-	}
-}
---
+++ java/jlowfuse/async/tasks/defaultImpl/Init.java
@@ -0,0 +1,12 @@
+package jlowfuse.async.tasks.defaultImpl;
+
+import jlowfuse.async.tasks.JLowFuseTask;
+
+public class Init extends JLowFuseTask implements jlowfuse.async.tasks.Init {
+	public void attributes() {
+	}
+
+    public void run() {
+	    super.run();
+    }	
+}
