f8db314bf59b8778e772a52a9afc02f4bf4a206f
Marcel Lauhoff <ml@irq0.org>
2011-10-07 02:31:38 +0200

    Objectfs: configured the task implementations to use

--- java/objectfs/AsyncObjectFs.java
+++ java/objectfs/AsyncObjectFs.java
@@ -8,6 +8,7 @@ import java.util.concurrent.TimeUnit;
 import jlowfuse.JLowFuse;
 import jlowfuse.JLowFuseArgs;
 import jlowfuse.async.DefaultTaskImplementations;
+import jlowfuse.async.TaskImplementations;
 import objectfs.async.ObjectFsContext;
 import fuse.Fuse;
 import fuse.SWIGTYPE_p_fuse_chan;
@@ -30,8 +31,19 @@ public class AsyncObjectFs {
 		DefaultTaskImplementations<ObjectFsContext> impls = 
 			new DefaultTaskImplementations<ObjectFsContext>();
 
-		//impls.initImpl
-
+		impls.getattrImpl = TaskImplementations.getImpl("objectfs.async.tasks.Getattr");
+		impls.initImpl = TaskImplementations.getImpl("objectfs.async.tasks.Init");
+		impls.lookupImpl = TaskImplementations.getImpl("objectfs.async.tasks.Lookup");
+		impls.mkdirImpl = TaskImplementations.getImpl("objectfs.async.tasks.Mkdir");
+		impls.mknodImpl = TaskImplementations.getImpl("objectfs.async.tasks.Mknod");
+		impls.readImpl = TaskImplementations.getImpl("objectfs.async.tasks.Read");
+		impls.readdirImpl = TaskImplementations.getImpl("objectfs.async.tasks.Readdir");
+		impls.rmdirImpl = TaskImplementations.getImpl("objectfs.async.tasks.Rmdir");
+		impls.setattrImpl = TaskImplementations.getImpl("objectfs.async.tasks.Setattr");
+		impls.statfsImpl = TaskImplementations.getImpl("objectfs.async.tasks.Statfs");
+		impls.unlinkImpl = TaskImplementations.getImpl("objectfs.async.tasks.Unlink");
+		impls.writeImpl = TaskImplementations.getImpl("objectfs.async.tasks.Write");
+		
 		ExecutorService service = new ThreadPoolExecutor(10, 20, 5,
 				TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(100));
 
