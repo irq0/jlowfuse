92d95ece17e2fff09541b3f9248b33131dbef805
Marcel Lauhoff <ml@irq0.org>
2012-08-28 02:08:46 +0200

    Add licence header

--- java/jlowfuse/OpsProxy.java
+++ java/jlowfuse/OpsProxy.java
@@ -1,10 +1,20 @@
-/**
- * Interface for proxy classes. This methods are expected by the underlying
- * native code.
+/*
+ * Copyright (c) 2011 Marcel Lauhoff.
  * 
- * The comments in this files are basically forwarded from fuse_lowlevel_ops
- *
- * @author Marcel Lauhoff <ml@irq0.org>
+ * This file is part of jlowfuse.
+ * 
+ * jlowfuse is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * jlowfuse is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with jlowfuse.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package jlowfuse;
